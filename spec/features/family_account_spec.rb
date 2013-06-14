9b6cc7ba0f2c31a0e0cedf315c618b5a363838b7
Jon Rish <jon.m.rish@gmail.com>
2013-06-14 13:08:38 -0400

    complete family account management w/ devise

--- spec/features/family_account_spec.rb
+++ spec/features/family_account_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 # 1) I want to be able to create an account
 # so I can keep track of activity and interests for me and my kids. 
 
-describe 'a user creates a family account' do
+feature 'a user creates a family account' do
 
-  it 'allows user to sign up' do
+  scenario 'a user signs up' do
     visit family_accounts_path
     click_link 'Sign Up Today!'
     expect(page).to have_content 'Let\'s get started!'
@@ -19,11 +19,11 @@ describe 'a user creates a family account' do
     select '1', from: 'family_account_date_of_birth_3i'
     select '2000', from: 'family_account_date_of_birth_1i'
 
-    click_button "Create/Update Family Account"
+    click_button "Create Family Account"
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
-  it 'allows a user to sign in and sign out' do
+  scenario 'a user signs in and signs out' do
     FactoryGirl.create(:family_account)
     visit family_accounts_path
     click_link 'Log In'
@@ -44,16 +44,23 @@ describe 'a user creates a family account' do
     click_button 'Sign in'
   end
 
-  it 'allows a user to edit account information' do
+  scenario 'a user edits their account information' do
     sign_in
     click_link 'My Account'
     click_link 'Edit My Family Account'
     expect(page).to have_content 'You can make changes to your account here.'
     fill_in 'Email', with: 'test1@test.com'
     fill_in 'Current password', with: 'testtest'
-    click_button 'Create/Update Family Account'
-    save_and_open_page
+    click_button 'Update Family Account'
     expect(page).to have_content 'You updated your account successfully.'
+  end
+
+  scenario 'a user deletes their account' do
+    sign_in
+    visit edit_family_account_registration_path
+    click_link 'Delete My Account'
+    expect(page).to have_content 'Your account has been successfully deleted.'
+    expect(current_path).to eql root_path
   end
 
   # it 'allows a user to view their account information' do
