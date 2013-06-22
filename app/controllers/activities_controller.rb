2ee3e40042b9ec347074f3d61ef33f468af116c9
Jon Rish <jon.m.rish@gmail.com>
2013-06-21 21:12:48 -0400

    complete view individual activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,4 +3,8 @@ class ActivitiesController < ApplicationController
     @search = Activity.search(params[:q])
     @activities = @search.result
   end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
 end
