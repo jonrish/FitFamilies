dedad91f34ceaee6461064e0634a8e428f4b82ce
Jon Rish <jon.m.rish@gmail.com>
2013-06-24 22:22:33 -0400

    complete add item to activity log

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -8,4 +8,19 @@ class ActivityLogsController < ApplicationController
 	def show
 		@activity_log = ActivityLog.find(params[:id])
 	end
+
+	def create
+		@child_account = ChildAccount.find(params[:child_account_id])
+		@activity_log = @child_account.activity_logs.build(params[:activity_log])
+
+		if @activity_log.save
+			redirect_to child_account_activity_logs_path(@child_account)
+			flash[:notice] = 'The activity has been added to your log'
+		else
+			redirect_to child_account_favorite_activities_path(@child_account)
+			flash[:notice] = 'Something went wrong, the activity was not added to your log'
+		end
+	end
 end
+
+
