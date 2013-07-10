718dfc8bda8ccf7312ba66c0112f8e67f5065f6d
Jon Rish <jon.m.rish@gmail.com>
2013-07-10 10:13:46 -0400

    modify app name in email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Bt3::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'fitfamilies-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'fitfamilies.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -72,7 +72,7 @@ Bt3::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "fitfamilies-staging.herokuapp.com",
+    domain: "fitfamilies.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
