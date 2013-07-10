cd6e23e8438193bbd677cdb3d7859a8879018115
Jon Rish <jon.m.rish@gmail.com>
2013-07-10 04:15:37 -0400

    modify email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Bt3::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "myapp.herokuapp.com",
+    domain: "fitfamilies-staging.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
